092144343c3650ddcb6861e4d98907a92e002097
adegregorio86 <korn1994@gmail.com>
2017-02-24 01:17:52 -0300

    Archivos de Buses de Mercedes

--- src/logica/negocio/Bus.java
+++ src/logica/negocio/Bus.java
@@ -1,5 +1,46 @@
 package logica.negocio;
+import logica.valueobjects.*;
+
+import java.io.*;
+import java.awt.*;
 
 public class Bus {
+	
+	private String matricula;
+	private String marca;
+	private int capacidad;
+	private Excursiones excursionesAsignadas;
+
+public Bus(String matricula,String marca, int capacidad){
+	
+	this.matricula = matricula;
+	this.marca = marca;
+	this.capacidad = capacidad;
+}
+
+public String getMatricula(){
+	return matricula;
+}
+
+public String getMarca(){
+	return marca;
+}
+public int getCapacidad(){
+	return capacidad;
+}
+public int getCantidadExcursiones(){
+	return excursionesAsignadas.lenght();
+}
+public List<VOExcursionSalida> listarExcursionesAsignadas(){
+	return excursionesAsignadas.listarExcursiones();
+}
+public void agregarExcursion(Excursion excursion){
+	return excursionesAsignadas.agregarExcrusion();
+}
+public void borrarExcursion(String matricula){
+	Excursion auxiliar = Excursiones.obtenerExcursion(matricula);
+	excursionesAsignadas.delete(auxiliar);
+}
+
 
 }
