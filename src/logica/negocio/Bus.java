6b6abe8311d6fa3f5530de2abea89d2c2ef1ef21
mmbgit <mmboffano@gmail.om>
2017-03-04 13:43:47 -0300

    cambios en buses

--- src/logica/negocio/Bus.java
+++ src/logica/negocio/Bus.java
@@ -3,45 +3,67 @@ package logica.negocio;
 import java.io.Serializable;
 import java.util.List;
 
+import logica.valueobjects.VOBusSalida;
 import logica.valueobjects.VOExcursionSalida;
 
 @SuppressWarnings("serial")
 public class Bus implements Serializable {
-	
+
 	private String matricula;
 	private String marca;
 	private int capacidad;
 	private Excursiones excursionesAsignadas;
 
-public Bus(String matricula,String marca, int capacidad){
+	public Bus(String matricula,String marca, int capacidad){
+
+		this.matricula = matricula;
+		this.marca = marca;
+		this.capacidad = capacidad;
+	}
+
+	public String getMatricula(){
+		return matricula;
+	}
+
+	public String getMarca(){
+		return marca;
+	}
+	public int getCapacidad(){
+		return capacidad;
+	}
+	public int getCantidadExcursiones(){
+		return excursionesAsignadas.tamanio();
+	}
+	public List<VOExcursionSalida> listarExcursionesAsignadas(){
+		return excursionesAsignadas.listarExcursiones();
+	}
+	public void agregarExcursion(Excursion excursion){
+		excursionesAsignadas.agregar(excursion);
+	}
+	public void borrarExcursion(String codigo){
+		excursionesAsignadas.quitar(codigo);
+	}
+	@Override
+	public boolean equals(Object obj ){
+		return obj != null
+				&& obj instanceof Bus
+				&& ((Bus)obj).getMatricula().equals(matricula);
+
+	}
 	
-	this.matricula = matricula;
-	this.marca = marca;
-	this.capacidad = capacidad;
-}
+	public boolean disponiblePara(Excursion excursion){
+		
+		boolean disponible = excursionesAsignadas.hayColisionCon(excursion);
+		
+		
+		
+		
+	}
 
-public String getMatricula(){
-	return matricula;
-}
-
-public String getMarca(){
-	return marca;
-}
-public int getCapacidad(){
-	return capacidad;
-}
-public int getCantidadExcursiones(){
-	return excursionesAsignadas.tamanio();
-}
-public List<VOExcursionSalida> listarExcursionesAsignadas(){
-	return excursionesAsignadas.listarExcursiones();
-}
-public void agregarExcursion(Excursion excursion){
-	excursionesAsignadas.agregar(excursion);
-}
-public void borrarExcursion(String codigo){
-	excursionesAsignadas.quitar(codigo);
-}
+	public VOBusSalida voSalida() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 
 }
--- src/logica/negocio/Buses.java
+++ src/logica/negocio/Buses.java
@@ -1,9 +1,14 @@
 package logica.negocio;
+
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 import logica.valueobjects.VOBusSalida;
+import logica.valueobjects.VOExcursionSalida;
 
 
 @SuppressWarnings("serial")
@@ -17,41 +22,88 @@ public class Buses extends Diccionario<String, Bus> implements Serializable {
 		poner(bus.getMatricula(), bus);
 	}
 
-	public Bus obtenerBusDisponible(Date fechaHoraPartida, Date fechaHoraRegreso){
-		/*
-		Bus auxiliar = buses.info;
-		for(int i=0;i<auxiliar.excursionesAsignadas.lenght();i++){
-			exAuxiliar = auxiliar.excursionesAsignadas[i];
-			if(exAuxiliar.fechaHoraPartida > fechaHoraRegreso || exAuxiliar.fechaHoraRegreso < fechaHoraPardida)
-				return auxiliar;
-			else
-				return obtenerBusDisponible( fechaHoraPartida, fechaHoraRegreso);
-		}*/
-		throw new UnsupportedOperationException("Metodo no implementado, aun");
+	public Bus obtenerBusDisponible(Excursion excursion){
+
+		Iterator<Bus> iteradorBus = iterador();
+		Bus bus;
+		while (iteradorBus.hasNext()) {
+			bus = iteradorBus.next();
+			if (bus.disponiblePara(excursion)  ){
+				return bus;
+			}
+		}
+		return null;
 		
 	}
 	
 	public Bus obtenerOtroBusDisponible(Excursion excursion) {
+		
+		
+		Iterator<Bus> iteradorBus = iterador();
+		Bus bus;
+		while (iteradorBus.hasNext()) {
+			bus = iteradorBus.next();
+			if (!bus.equals(excursion.getBus()) && bus.disponiblePara(excursion)  ){
+				return bus;
+			}
+		}
+		return null;
 		// TODO: Devolver el primer bus que, ademas de estar disponible, tenga
 		// capacidad suficiente para la excursion y sea diferente del bus que ya tiene asignado.
-		throw new UnsupportedOperationException("Metodo no implementado, aun");
+		//throw new UnsupportedOperationException("Metodo no implementado, aun");
 	}
 	
-	public boolean hayBusDisponible(Date fechaHoraPartida, Date fechaHoraRegreso){
-		/*boolean hayBus= false;
-		Bus auxiliar = buses.info;
-		for(int i=0;i<auxiliar.excursionesAsignadas.lenght();i++){
-			exAuxiliar = auxiliar.excursionesAsignadas[i];
-			if(exAuxiliar.fechaHoraPartida > fechaHoraRegreso || exAuxiliar.fechaHoraRegreso < fechaHoraPardida)
-				hayBus =  true;
-			else
-				return hayBusDisponible( fechaHoraPartida, fechaHoraRegreso);
-		}*/
-		throw new UnsupportedOperationException("Metodo no implementado, aun");
-	}
+	/*public boolean hayBusDisponible(Date fechaHoraPartida, Date fechaHoraPartida){
+		boolean haybus= false;
+		boolean colisiona = true;
+		Bus bus;
+		Iterator<Bus> iteradorBus = iterador();
+		List<VOBusSalida> busesSalida = new ArrayList<VOBusSalida>();
+		
+		while (iteradorBus.hasNext()) {
+			bus = iteradorBus.next();
+			Excursion excursion;
+			Iterator<Excursion> iteradorExcursiones = this.iterador();
+			List<VOExcursionSalida> excursionesSalida = new ArrayList<VOExcursionSalida>();
+			
+			while (iteradorExcursiones.hasNext()) {
+				excursion = iteradorExcursiones.next();
+				colisiona = fechaHoraRegreso.compareTo(excursion.fechaHoraPartida) > 0
+						&& fechaHoraPartida.compareTo(excursion.fechaHoraRegreso) < 0;
+				if (colisiona == false){
+					haybus = true;
+		
+				}		 
+			}
+		}
+		return haybus;
+		
+	
+		//throw new UnsupportedOperationException("Metodo no implementado, aun");
+
+	
+	}*/
 	
 	public List<VOBusSalida> listarBuses() {
-		throw new UnsupportedOperationException("Metodo no implementado, aun");
+		//throw new UnsupportedOperationException("Metodo no implementado, aun");
+		Bus bus;
+		Iterator<Bus> iteradorBus = iterador();
+		List<VOBusSalida> busesSalida = new ArrayList<VOBusSalida>();
+		
+		while (iteradorBus.hasNext()) {
+			bus = iteradorBus.next();
+			busesSalida.add(bus.voSalida());
+		}
+		
+		Collections.sort(busesSalida);
+		
+		return busesSalida;
+		
+		
 	}
+
+
+	
+}	
 
 }
