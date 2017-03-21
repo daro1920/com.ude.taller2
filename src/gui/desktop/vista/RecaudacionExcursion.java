4e190b405f4a0c26f535ed0edb6490be28f5ccd9
adegregorio86 <korn1994@gmail.com>
2017-03-20 21:21:32 -0300

    Recaudacion Excursion terminada

--- src/gui/desktop/vista/RecaudacionExcursion.java
+++ src/gui/desktop/vista/RecaudacionExcursion.java
@@ -1,103 +1,139 @@
 package gui.desktop.vista;
 
-import java.awt.Color;
-import java.awt.EventQueue;
-import java.awt.Font;
+import gui.desktop.control.ControladorRecaudacionExcursion;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.math.BigDecimal;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class RecaudacionExcursion {
+	
+	private ControladorRecaudacionExcursion controlador;
+	private boolean inicioFallido;
+	private boolean errorDeCarga;
 
 	private JFrame frame;
-	private JTextField textField;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					RecaudacionExcursion window = new RecaudacionExcursion();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private JTextField textFieldRecaudacion;
+	private JTextField textFieldCodigoExcursion;
 
 	/**
 	 * Create the application.
 	 */
 	public RecaudacionExcursion() {
 		initialize();
+		inicioFallido = false;
+		controlador = new ControladorRecaudacionExcursion(this);
 	}
+	
+    // #########################################################################
+    // # METODOS PUBLICOS                                                      #
+    // #########################################################################
+
+	public void actuarAnteErrorConexionInicial() {
+		inicioFallido = true;
+		JOptionPane.showMessageDialog(frame,
+                "No fue posible conectarse al servidor.",
+                "Error de conexión",
+                JOptionPane.ERROR_MESSAGE);
+	}
+
+	public void actuarAnteErrorConfiguracionInicial() {
+		inicioFallido = true;
+		JOptionPane.showMessageDialog(frame,
+                "No fue posible cargar la configuración.",
+                "Error de configuración",
+                JOptionPane.ERROR_MESSAGE);
+	}
+
+	public void actuarAnteErrorConexionMetodo() {
+		errorDeCarga = true;
+		JOptionPane.showMessageDialog(frame,
+                "No fue posible conectarse al servidor.\n"
+                + "Intentelo más tarde",
+                "Error de conexión",
+                JOptionPane.ERROR_MESSAGE);
+	}
+
+	public void actuarAnteErrorNoExisteExcursion() {
+		errorDeCarga = true;
+		JOptionPane.showMessageDialog(frame,
+                "No existe una excursión con el codigo dado.",
+                "Excursión inexistente",
+                JOptionPane.ERROR_MESSAGE);
+	}
+
+	// #########################################################################
+    // # INITIALIZE Y SET VISIBLE                                              #
+    // #########################################################################
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setTitle("Recaudacion Excursion");
-		frame.setBounds(100, 100, 450, 243);
+		frame.setResizable(false);
+		frame.setTitle("Recaudaci\u00F3n");
+		frame.setBounds(100, 100, 300, 155);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblSeleccioneLaExcursion = new JLabel("Seleccione la excursion:");
-		lblSeleccioneLaExcursion.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblSeleccioneLaExcursion.setBounds(12, 13, 172, 16);
-		frame.getContentPane().add(lblSeleccioneLaExcursion);
-		
-		JList list = new JList();
-		list.setBounds(22, 42, 398, 30);
-		frame.getContentPane().add(list);
-		
 		JButton btnCalcularRecaudacion = new JButton("Calcular Recaudacion");
-		btnCalcularRecaudacion.setForeground(new Color(0, 0, 128));
-		btnCalcularRecaudacion.setFont(new Font("Tahoma", Font.BOLD, 13));
-		btnCalcularRecaudacion.setBounds(22, 163, 172, 25);
+		btnCalcularRecaudacion.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnCalcularRecaudacionActionPerformed(e);
+			}
+		});
+		btnCalcularRecaudacion.setBounds(12, 82, 270, 25);
 		frame.getContentPane().add(btnCalcularRecaudacion);
 		
-		JButton btnCancelar = new JButton("Cancelar");
-		btnCancelar.setForeground(new Color(0, 0, 128));
-		btnCancelar.setFont(new Font("Tahoma", Font.BOLD, 13));
-		btnCancelar.setBounds(248, 163, 172, 25);
-		frame.getContentPane().add(btnCancelar);
-		
-		JLabel lblRecaudacion = new JLabel("Recaudacion:");
-		lblRecaudacion.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblRecaudacion.setBounds(12, 96, 113, 16);
+		JLabel lblRecaudacion = new JLabel("Recaudaci\u00F3n");
+		lblRecaudacion.setBounds(12, 42, 97, 16);
 		frame.getContentPane().add(lblRecaudacion);
 		
-		textField = new JTextField();
-		textField.setBounds(104, 96, 116, 22);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		textFieldRecaudacion = new JTextField();
+		textFieldRecaudacion.setEditable(false);
+		textFieldRecaudacion.setBounds(121, 39, 161, 22);
+		frame.getContentPane().add(textFieldRecaudacion);
+		textFieldRecaudacion.setColumns(10);
+		
+		JLabel lblCodigoExcursion = new JLabel("C\u00F3digo Excursi\u00F3n");
+		lblCodigoExcursion.setBounds(12, 13, 97, 16);
+		frame.getContentPane().add(lblCodigoExcursion);
+		
+		textFieldCodigoExcursion = new JTextField();
+		textFieldCodigoExcursion.setBounds(121, 10, 161, 22);
+		frame.getContentPane().add(textFieldCodigoExcursion);
+		textFieldCodigoExcursion.setColumns(10);
 	}
 
 	/* Indico si deseo que la ventana sea visible o no */
 	public void setVisible (boolean visible) {
-		frame.setVisible(visible);
+		if (inicioFallido) {
+			frame.dispose();
+		} else {
+			frame.setVisible(visible);
+		}
 	}
 
-	public void actuarAnteErrorConexion() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("No implementado, aun");
-	}
+	// #########################################################################
+    // # EVENT HANDLERS                                                        #
+    // #########################################################################
 
-	public void actuarAnteErrorConfiguracion() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("No implementado, aun");
-	}
-
-	public void actuarAnteErrorNoExisteExcursion() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("No implementado, aun");
+	protected void btnCalcularRecaudacionActionPerformed(ActionEvent e) {
+		errorDeCarga = false;
+		String codigo = textFieldCodigoExcursion.getText();
+		BigDecimal recaudacion = controlador.recaudacionExcursion(codigo);
+		if (errorDeCarga) {
+			textFieldRecaudacion.setText("");
+		} else {
+			textFieldRecaudacion.setText(recaudacion.toPlainString());
+		}
 	}
 
 }
